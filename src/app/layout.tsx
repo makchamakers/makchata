cbea6cf46f60e0127e5266662185f4cae66558ac
sunaero <sunaerocket@gmail.com>
2023-10-21 21:24:13 +0900

    chore: 스타일드 컴포넌트 설치 및 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import StyledComponentsRegistry from '../../lib/registry';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -13,7 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
