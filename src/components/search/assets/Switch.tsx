09cbb437727904bd4a8b8c25c634677ee0ee81b0
Yang-ah <yangah0409@gmail.com>
2023-10-22 14:24:27 +0900

    asset: searchPage SVG components 추가

---
+++ src/components/search/assets/Switch.tsx
@@ -0,0 +1,27 @@
+const Switch = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+    >
+      <path
+        d="M1 7L7 1L7 19"
+        stroke="#BBBBBB"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M19 13L13 19V1"
+        stroke="#BBBBBB"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+export default Switch;
---
+++ src/components/search/assets/index.ts
@@ -0,0 +1,5 @@
+export { default as MyHomeSVG } from './MyHome';
+export { default as LocationSVG } from './LocationIcon';
+export { default as SettingSVG } from './Setting';
+export { default as SwitchSVG } from './Switch';
+export { default as XSVG } from './X';
