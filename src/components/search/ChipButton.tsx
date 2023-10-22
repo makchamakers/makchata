7ade978e4292783e7cff0955c8e3d9afa86e5800
Yang-ah <yangah0409@gmail.com>
2023-10-22 14:51:44 +0900

    Design: Search Header 퍼블리싱

---
+++ src/components/search/ChipButton.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import { MyHomeSVG } from './assets';
+
+interface IChipButton {
+  text: string;
+  onClick: () => void;
+}
+
+const ChipButton = ({ text, onClick }: IChipButton) => {
+  return (
+    <Button onClick={onClick}>
+      <MyHomeSVG />
+      <p>{text}</p>
+    </Button>
+  );
+};
+
+const Button = styled.button`
+  display: flex;
+  padding: 4px 8px;
+  align-items: center;
+  border-radius: 30px;
+  background: #e8ebed;
+  border: none;
+  font-size: 12px;
+`;
+
+export default ChipButton;
