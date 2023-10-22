4fccaaad4ee98fad3b4066d60248d33ce55dda4d
sunaero <sunaerocket@gmail.com>
2023-10-23 01:16:36 +0900

    refactor: 네비게이션바 구조 변경

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,47 +1,38 @@
 'use client';
 
-import { useRouter, usePathname } from 'next/navigation';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import HomeIcon from '@/components/HomeIcon';
 import PersonIcon from '@/components/PersonIcon';
 import SettingIcon from '@/components/SettingIcon';
 import styled from 'styled-components';
 
-type PagePath = '/setting' | '/' | '/mypage';
-
 export default function NavigationBar() {
-  const router = useRouter();
-  const pathname = usePathname();
-
-  const isSetting = pathname === '/setting';
-  const isHome = pathname === '/';
-  const isMyPage = pathname === '/mypage';
-
-  const handleNavItemClick = (path: PagePath) => {
-    router.push(path);
-  };
+  const activePath = usePathname();
+  const isSetting = activePath === '/setting';
+  const isHome = activePath === '/';
+  const isMyPage = activePath === '/mypage';
 
   return (
     <Container>
-      <NavMenu>
-        <NavItem
-          $active={isSetting}
-          onClick={() => handleNavItemClick('/setting')}
-        >
+      <Link href="/setting">
+        <NavLink $active={isSetting}>
           <SettingIcon color={isSetting ? '#FF8048' : '#BBBBBB'} />
           <span>설정</span>
-        </NavItem>
-        <NavItem $active={isHome} onClick={() => handleNavItemClick('/')}>
+        </NavLink>
+      </Link>
+      <Link href="/">
+        <NavLink $active={isHome}>
           <HomeIcon color={isHome ? '#FF8048' : '#BBBBBB'} />
           <span>홈</span>
-        </NavItem>
-        <NavItem
-          $active={isMyPage}
-          onClick={() => handleNavItemClick('/mypage')}
-        >
+        </NavLink>
+      </Link>
+      <Link href="/mypage">
+        <NavLink $active={isMyPage}>
           <PersonIcon color={isMyPage ? '#FF8048' : '#BBBBBB'} />
           <span>마이페이지</span>
-        </NavItem>
-      </NavMenu>
+        </NavLink>
+      </Link>
     </Container>
   );
 }
@@ -52,20 +43,15 @@ const Container = styled.nav`
   left: 0;
   width: 100%;
   height: 88px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 8px 64px 32px;
   background-color: #fff;
   border-top: 1px solid #ddd;
 `;
 
-const NavMenu = styled.ul`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  height: 100%;
-  padding: 8px 64px 32px;
-  list-style: none;
-`;
-
-const NavItem = styled.li<{ $active: boolean }>`
+const NavLink = styled.a<{ $active: boolean }>`
   position: relative;
   display: flex;
   flex-direction: column;
