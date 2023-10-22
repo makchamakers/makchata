dca512ae7a93cdf23f4a5a61d13d2c40d28aabce
sunaero <sunaerocket@gmail.com>
2023-10-22 11:42:39 +0900

    chore: tanstack query 설정

---
+++ lib/Providers.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import React from 'react';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+function Providers({ children }: Props) {
+  const [client] = React.useState(
+    new QueryClient({
+      defaultOptions: {
+        // react-query 전역 설정
+        queries: {
+          refetchOnWindowFocus: false,
+          retry: false,
+        },
+      },
+    })
+  );
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default Providers;
