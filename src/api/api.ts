dc4d1ed83378f3e9e52162f97c8a67fd1d431c55
김인태 <dlsxody1@naver.com>
2023-10-23 14:45:50 +0900

    검색 api 요청 추가

--- src/api/api.ts
+++ src/api/api.ts
@@ -14,3 +14,23 @@ export const getSearchResult = async (search: string, departure: string) => {
   );
   return res.json();
 };
+
+/**
+ *
+ * @param sx 출발지 x좌표
+ * @param sy 출발지 y좌표
+ * @param ex 목적지 x좌표
+ * @param ey 목적지 y좌표
+ * @returns
+ */
+export const getUserRoute = async (
+  sx: string,
+  sy: string,
+  ex: string,
+  ey: string
+) => {
+  const res = await fetch(
+    `http://localhost:8080/destination?sx=${sx}&sy=${sy}&ex=${ex}&ey=${ey}`
+  );
+  return res.json();
+};
