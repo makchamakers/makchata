8575bafbd7c7f7e6767896721f100aa9026c17f1
김진영 <kimjin-young@gimjin-yeong-ui-MacBookAir.local>
2023-11-12 16:13:33 +0900

    feat : 타이머 게이지 작업 (#53)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,9 +32,10 @@ export default function Home() {
   };
 
   function updateCurrentTime() {
-    // 막차 시간 임의로 설정
-    const makchaTime = new Date('Thu Nov 07 2023 23:59:59 GMT+0900');
+    // 막차 시간 임의로 설정ß
+    const makchaTime = new Date('Sun Nov 12 2023 22:59:59 GMT+0900');
     const currentTime = new Date();
+    console.log(currentTime);
 
     // 두 시간 사이의 차이 (밀리초)
     const timeDifferenceInMilliseconds =
@@ -57,18 +58,14 @@ export default function Home() {
     // 게이지 계산하기
     // 알람 시간을 Date 객체로 변환
     const alarmSettingTime2 = new Date(alarmSettingTime);
-
+    // 게이지 전체값 계산
     const leftTime = makchaTime.getTime() - alarmSettingTime2.getTime();
+    // 게이지 현재값 계산
+    const currentLeftTime =
+      ((currentTime.getTime() - alarmSettingTime2.getTime()) / leftTime) * 100;
 
-    //현재 남은 시간 게이지
-    const leftTimeGage = Math.floor(
-      (timeDifferenceInMilliseconds / leftTime) * 100
-    );
-
-    setTimeGage(leftTimeGage);
-    console.log('시간 차이 : ' + timeDifferenceInMilliseconds);
-    console.log(`남은 시간: ${leftTime}`);
-    console.log(`타임 게이지: ${timeGage}`);
+    console.log(`타임 게이지: ${currentLeftTime}`);
+    setTimeGage(currentLeftTime);
   }
 
   // 컴포넌트가 처음 렌더링될 때 실행
@@ -88,7 +85,6 @@ export default function Home() {
   //게이지 기본 속성값
   const RADIUS = 50;
   const CIRCUMFERENCE = 2 * Math.PI * RADIUS; // 둘레 길이
-  // const dashoffset = CIRCUMFERENCE * (1 - 0);
 
   return (
     <Container>
@@ -103,6 +99,7 @@ export default function Home() {
           width={110}
           height={96}
           alt="막차타일러스트"
+          priority
         />
       </TitleWrapper>
 
@@ -151,10 +148,8 @@ export default function Home() {
                 r={RADIUS}
                 strokeWidth="15"
                 fill="transparent"
-                stroke="#FFD9C9"
-                strokeDasharray={`CIRCUMFERENCE * 0.65} ${
-                  CIRCUMFERENCE * 0.35
-                }`}
+                stroke="#ddd"
+                strokeDasharray={CIRCUMFERENCE}
                 strokeDashoffset="0"
                 strokeLinecap="round"
               />
@@ -164,9 +159,9 @@ export default function Home() {
                 cx="60"
                 cy="60"
                 r={RADIUS}
-                stroke={alarm === false ? '#ddd' : 'FFD9C9'}
+                stroke={alarm === false ? '#ddd' : '#FFD9C9'}
                 strokeWidth="15"
-                strokeDasharray="0"
+                strokeDasharray={CIRCUMFERENCE}
                 strokeDashoffset={timeGage}
                 fill="none"
                 strokeLinecap="round"
@@ -185,13 +180,13 @@ export default function Home() {
                 alt="alert이미지"
                 width={40}
                 height={40}
+                priority
               />
               <p>막차 경로를 설정해주세요</p>
             </InfoBox>
           ) : (
             <>
               <h3>오늘 꼭 타야하는 막차 경로</h3>
-              <PathDetail />
             </>
           )}
         </RouteWrap>
