12ed7ee1e8b09bb7251e32824b96b48306184a83
eun0leee <eun0leee@gmail.com>
2023-11-17 12:00:36 +0900

    bug : warning 해결로 og카드 미작동 다시 코드 복구 (#55)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import './globals.css';
 
 // TODO: 실제 도메인으로 변경해야함
 export const metadata: Metadata = {
-  metadataBase: new URL(process.env.URL ?? 'http://localhost:3000'),
   title: 'Makchata',
   description: 'Makchata by MakchaMakers',
   icons: {
@@ -17,7 +16,7 @@ export const metadata: Metadata = {
       '막차타는 막차를 타고 싶은 사람들을 위한 서비스입니다. 귀가길에 막차를 타려면 언제 일어나야 하는지 진동과 메시지로 알려드립니다. 어떻게? 직관적이게 따뜻하게 귀엽게 맞춤형으로!',
     type: 'website',
     locale: 'ko_KR',
-    url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+    url: 'https://makchata.vercel.app/',
     images: [
       {
         url: '/assets/images/img_thumbnail.png',
