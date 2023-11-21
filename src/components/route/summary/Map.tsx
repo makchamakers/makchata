875ba82fd926f4cd94c51c504de8c2d2a0cec992
eun0leee <eun0leee@gmail.com>
2023-11-22 01:13:16 +0900

    fix : 지도띄우는 함수 컴포넌트 외부로 분리 (#40)

--- src/components/route/summary/Map.tsx
+++ src/components/route/summary/Map.tsx
@@ -12,6 +12,71 @@ declare global {
   }
 }
 
+// 지도 띄우기
+const onLoadKakaoAPI = (pathDetailLocations: PathDetailResponseProps[]) => {
+  window.kakao.maps.load(() => {
+    // 초기 지도화면
+    const mapContainer = document.getElementById('map');
+    const mapOption = {
+      center: new window.kakao.maps.LatLng(37.5190581, 127.0238072), // 지도의 중심좌표
+      level: 3, // 지도의 확대 레벨
+    };
+
+    const map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+    // 지도 확대, 축소 이벤트
+    const zoomControl = new window.kakao.maps.ZoomControl();
+    map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
+
+    window.kakao.maps.event.addListener(map, 'zoom_changed', () => {
+      map.getLevel();
+    });
+
+    // 다중 마커 표시
+    const positions = [
+      {
+        title: '출발지',
+        latlng: new window.kakao.maps.LatLng(37.5190581, 127.0238072),
+        src: 'https://t1.daumcdn.net/localimg/localimages/07/2018/pc/flagImg/blue_b.png',
+      },
+      {
+        title: '도착지',
+        latlng: new window.kakao.maps.LatLng(37.464007, 126.9522394),
+        src: 'https://t1.daumcdn.net/localimg/localimages/07/2018/pc/flagImg/red_b.png',
+      },
+    ];
+
+    // 마커 이미지 크기
+    const imageSize = new window.kakao.maps.Size(37, 42);
+
+    // 지도 중심좌표 및 범위 재설정
+    const bounds = new window.kakao.maps.LatLngBounds();
+
+    for (const position of positions) {
+      // 마커 이미지 생성
+      const markerImage = new window.kakao.maps.MarkerImage(
+        position.src,
+        imageSize
+      );
+
+      // 마커 생성
+      const marker = new window.kakao.maps.Marker({
+        map, // 마커를 표시할 지도
+        position: position.latlng, // 마커를 표시할 위치
+        title: position.title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됨
+        image: markerImage, // 마커 이미지
+      });
+
+      marker.setMap(map);
+      bounds.extend(position.latlng);
+    }
+
+    map.setBounds(bounds);
+
+    console.log(pathDetailLocations);
+  });
+};
+
 export default function Map() {
   // 경로 상세 response 담는 state
   const [pathDetailLocations, setPathDetailLocations] = useState<
@@ -32,8 +97,6 @@ export default function Map() {
     fetchData();
   }, []);
 
-  console.log(pathDetailLocations);
-
   useEffect(() => {
     // 지도불러오는 script
     const kakaoMapScript = document.createElement('script');
@@ -41,70 +104,9 @@ export default function Map() {
     kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&autoload=false&libraries=services,clusterer,drawing`;
     document.head.appendChild(kakaoMapScript);
 
-    // 지도 띄우기
-    const onLoadKakaoAPI = () => {
-      window.kakao.maps.load(() => {
-        // 초기 지도화면
-        const mapContainer = document.getElementById('map');
-        const mapOption = {
-          center: new window.kakao.maps.LatLng(37.5190581, 127.0238072), // 지도의 중심좌표
-          level: 3, // 지도의 확대 레벨
-        };
-
-        const map = new window.kakao.maps.Map(mapContainer, mapOption);
-
-        // 지도 확대, 축소 이벤트
-        const zoomControl = new window.kakao.maps.ZoomControl();
-        map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
-
-        window.kakao.maps.event.addListener(map, 'zoom_changed', () => {
-          map.getLevel();
-        });
-
-        // 다중 마커 표시
-        const positions = [
-          {
-            title: '출발지',
-            latlng: new window.kakao.maps.LatLng(37.5190581, 127.0238072),
-            src: 'https://t1.daumcdn.net/localimg/localimages/07/2018/pc/flagImg/blue_b.png',
-          },
-          {
-            title: '도착지',
-            latlng: new window.kakao.maps.LatLng(37.464007, 126.9522394),
-            src: 'https://t1.daumcdn.net/localimg/localimages/07/2018/pc/flagImg/red_b.png',
-          },
-        ];
-
-        // 마커 이미지 크기
-        const imageSize = new window.kakao.maps.Size(37, 42);
-
-        // 지도 중심좌표 및 범위 재설정
-        const bounds = new window.kakao.maps.LatLngBounds();
-
-        for (const position of positions) {
-          // 마커 이미지 생성
-          const markerImage = new window.kakao.maps.MarkerImage(
-            position.src,
-            imageSize
-          );
-
-          // 마커 생성
-          const marker = new window.kakao.maps.Marker({
-            map, // 마커를 표시할 지도
-            position: position.latlng, // 마커를 표시할 위치
-            title: position.title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됨
-            image: markerImage, // 마커 이미지
-          });
-
-          marker.setMap(map);
-          bounds.extend(position.latlng);
-        }
-
-        map.setBounds(bounds);
-      });
-    };
-
-    kakaoMapScript.addEventListener('load', onLoadKakaoAPI);
+    kakaoMapScript.addEventListener('load', () =>
+      onLoadKakaoAPI(pathDetailLocations)
+    );
   }, []);
 
   return <Container id="map"></Container>;
